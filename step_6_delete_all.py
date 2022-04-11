9861900ae5da70b56a9e6b7c181038d8b249e107
alexamikaya <82936266+alexamikaya@users.noreply.github.com>
2022-04-11 19:28:42 +0300

    Update step_6_delete_all.py

--- step_6_delete_all.py
+++ step_6_delete_all.py
@@ -1,10 +1,11 @@
+import pandas as df 
+import tkinter.messagebox as mb
+import tkinter as tk
+from tkinter import ttk
+import csv
 def delete():
    
-    import pandas as df 
-    import tkinter.messagebox as mb
-    import tkinter as tk
-    from tkinter import ttk
-    import csv
+    
     window=tk.Tk()
     window.title('Calculation')
     frame_add_form = tk.Frame(window)
